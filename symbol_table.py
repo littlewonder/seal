3bef99910b4d1ddf570bf6ef89dce84ed8ebd8be
saiakhilaloor <saiakhil.aloor7@gmail.com>
2018-04-22 23:02:55 +0530

    Symbol table added

---
+++ symbol_table.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from token import lex
+from parser import parse
+import sys
+
+# Token types
+TOK_PRINT  = 0
+TOK_ID     = 1
+TOK_VAR    = 2
+TOK_INT    = 3
+TOK_FLOAT  = 4
+TOK_TYPE   = 5
+TOK_EQ     = 6
+TOK_PLUS   = 7
+TOK_MINUS  = 8
+TOK_STAR   = 9
+TOK_SLASH  = 10
+TOK_LPAREN = 11
+TOK_RPAREN = 12
+TOK_COLON  = 13
+TOK_WHILE  = 14
+TOK_DO     = 15
+TOK_DONE   = 16
+TOK_SEMI   = 17
+TOK_READ   = 18
+
+# AST nodes
+AST_DECL   = 0
+AST_ASSIGN = 1
+AST_PRINT  = 2
+AST_INT    = 3
+AST_FLOAT  = 4
+AST_ID     = 5
+AST_BINOP  = 6
+AST_WHILE  = 7
+AST_READ   = 8
+
+def build_symtab(ast):
+    """
+    Input : the AST of a mini program
+    Output: a dictionary mapping variable names to types
+
+    This procedure iterates over the declarations and adds them to a
+    symbol table (here, a dictionary that maps variable names to their
+    declared type).  If a variable is declared more than once, we
+    report an error.
+    """
+    symtab = {}
+    for decl in ast["decls"]:
+        if decl["id"] in symtab:
+            error("%s is already declared" % decl["id"])
+        else:
+            symtab[decl["id"]] = decl["type"]
+    return symtab
+
+
+def main():
+    src = sys.stdin.read()
+    toks = lex(src)
+    #printToken(toks)                   # source -> tokens
+    ast = parse(toks)
+    sym = build_symtab(ast)
+    print sym
+
+if __name__ == "__main__":
+    main()
