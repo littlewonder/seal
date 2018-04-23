61d9e2514ed1c222dccb0fd010282afabb1c5831
saiakhilaloor <saiakhil.aloor7@gmail.com>
2018-04-23 13:54:54 +0530

    demo c file

---
+++ demo.c
@@ -0,0 +1,27 @@
+a
+b
+n
+t
+#include <stdio.h>
+int main(void) {
+int a;
+int b;
+int n;
+int t;
+int t_1 = 0;
+a = t_1;
+int t_2 = 1;
+b = t_2;
+int t_3 = 10;
+n = t_3;
+while (n) { 
+t = a;
+a = b;
+int t_4 = b + t;
+b = t_4;
+int t_5 = 1;
+int t_6 = n - t_5;
+n = t_6;
+}
+printf("%d\n", a);
+}
